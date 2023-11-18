d8ee6dfce436ae49a5da09cc11130ad09b1865de
Onovo Christian <onovochristian19@gmail.com>
2023-11-18 08:58:44 +0100

    Final build v1

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// Leaftlet Map
+
+var map = L.map('map').setView([6.4483, 7.5139], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
